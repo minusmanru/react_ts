d5e83e6d8cfc000adedbcd2f56fe3e6636ac33f5
An <Andrew.Shinkarenko@datasolution.team>
2020-07-18 21:10:32 +0300

    add-changes-from-vs

--- src/components/Nawbar.tsx
+++ src/components/Nawbar.tsx
@@ -5,8 +5,8 @@ export const Navbar: React.FC = () => (
         <div className="nav-wrapper purple lighten-1 px1 ">
             <a href="/" className="brand-logo">React TS Logo</a>
             <ul  className="right hide-on-med-and-down">
-            <li><a href="/">Список </a></li>
-            <li><a href="/">Инфо</a></li>
+            <li><a href="/">Список</a></li>
+            <li><a href="/">Инфо </a></li>
             </ul>
         </div>
   </nav>
